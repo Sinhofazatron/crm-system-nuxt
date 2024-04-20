3d27e1cd7393e64efa2626d66e36928a67099473
Dmitry Sitnikov <betani.elvin@mail.ru>
2024-04-20 14:16:46 +0300

    first commit

---
+++ components/layout/Menu.vue
@@ -0,0 +1,24 @@
+<template lang="">
+  <div>
+    <NuxtLink 
+      v-for="item in MENU_DATA" 
+      :key="item.name" 
+      :to="item.url" 
+      class="flex items-center py-1 px-3 rounded-lg w-full hover:bg-gray-700 hover-shadow transition-all mb-2"
+    >
+      <Icon 
+        :name="item.icon" 
+        class="mr-3"
+    />
+      <span>{{ item.name }}</span>
+    </NuxtLink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { MENU_DATA } from './menu.data';
+</script>
+
+<styl>
+  
+</styl>
---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,19 @@
+<template >
+  <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+    <NuxtLink to="/" class="mb-10 flex justify-center">
+      <NuxtImg src="/logo.svg" alt="logo" width="50px"/>
+    </NuxtLink>
+    <button class=" absolute top-2 right-3 transition-colors hover:text-purple-200">
+      <Icon name="line-md:logout" size="20"/>
+    </button>
+    <LayoutMenu/>
+  </aside>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+  
+</style>
