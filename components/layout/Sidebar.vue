b5881e090cd7e605fa17bd8c97279cc82a00e2ad
Dmitry Sitnikov <betani.elvin@mail.ru>
2024-04-20 14:16:46 +0300

    first commit

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,15 @@
+<template >
+  <aside>
+    <NuxtLink to="/">
+      <NuxtImg src="/logo.svg" alt="logo" width="50px"/>
+    </NuxtLink>
+  </aside>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+  
+</style>
