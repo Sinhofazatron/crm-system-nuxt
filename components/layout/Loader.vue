860df34904fdbf5b8ac725495cd0c7c3254b6d62
Dmitry Sitnikov <betani.elvin@mail.ru>
2024-04-21 10:47:37 +0300

    Add DB

---
+++ components/layout/Loader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <img src="./loader.svg" class="wrapper" alt="loader" size="20"/>
+  </div>
+</template>
+<script lang="ts" setup>
+
+</script>
+<style>
+  .wrapper {
+    @apply bg-sidebar flex items-center justify-center w-screen h-screen
+  }
+</style>
