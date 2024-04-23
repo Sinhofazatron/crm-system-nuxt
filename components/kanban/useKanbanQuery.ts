38a73acbad1b728068dab4898eb7945b49fb62ae
Dmitry Sitnikov <betani.elvin@mail.ru>
2024-04-23 07:07:03 +0300

    Add card item

--- components/kanban/useKanbanQuery.ts
+++ components/kanban/useKanbanQuery.ts
@@ -4,7 +4,7 @@ import { KANBAN_DATA } from './kanban.data';
 import type { IDeal } from '~/types/deal.types';
 
 export function useKanbanQuery() {
-
+  
   return useQuery({
     queryKey: ['deals'],
     queryFn: () => DB.listDocuments(DB_ID, COLLECTION_DEALS),
@@ -12,6 +12,22 @@ export function useKanbanQuery() {
       const newBoard = [...KANBAN_DATA]
       const deals = data.documents as unknown as IDeal[]
       
+      for (const deal of deals) {
+        const column = newBoard.find(col => col.id === deal.status)
+
+        if (column) {
+          column.items.push({
+          $createdAt: deal.$createdAt,
+          id: deal.$id,
+          name: deal.name,
+          price: deal.price,
+          companyName: deal.customer.name,
+          status: deal.name
+          })
+        }
+      }
+
+      return newBoard
     }
   })
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="p-10">
     <h1 class="font-bolt text-2xl mb-10">CRM system</h1>
-    <div>
-      <UiCard class="mb-3 bg-slate-900" draggable="true">
-        <UiCardHeader role="button">name</UiCardHeader>
-        <UiCardContent>Company</UiCardContent>
-        <UiCardFooter>Date</UiCardFooter>
-      </UiCard>
+    <div v-if="isLoading">Loading...</div>
+    <div v-else>
+      <div class="grid grid-cols-5 gap-16">
+        <div v-for="(column, index) in data" :key="column.id">
+          <div class="rounded bg-slate-700 py-1 px-5 mb-2 text-center">
+            {{ column.name }}
+          </div>
+          <UiCard v-for="card in column.items" :key="card.id" class="mb-3 bg-slate-900" draggable="true">
+            <UiCardHeader role="button">
+              <UiCardTitle>{{ card.name }}</UiCardTitle>
+              <UiCardDescription>{{ convertCurrency(card.price) }}</UiCardDescription>
+            </UiCardHeader>
+            <UiCardContent>{{ card.companyName }}</UiCardContent>
+            <UiCardFooter>{{ dayjs(card.$createdAt).format('DD MMMM YYYY') }}</UiCardFooter>
+          </UiCard>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -14,6 +25,8 @@
 <script lang="ts" setup>
 import type { ICard, IColumn } from '~/components/kanban/kanban.type';
 import { useKanbanQuery } from '~/components/kanban/useKanbanQuery';
+import { convertCurrency } from '@/utils/convertCurrency'
+import dayjs from 'dayjs';
 
   useSeoMeta({
     title: 'Home'
@@ -21,8 +34,7 @@ import { useKanbanQuery } from '~/components/kanban/useKanbanQuery';
 
   const dragCardRef = ref<ICard | null>(null)
   const sourceColumnRef = ref<IColumn | null>(null)
-
-
+  const { data, isLoading, refetch } = useKanbanQuery()
 </script>
 
 <style>
